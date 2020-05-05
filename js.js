9a81d2cd4038f71afe95a9729367408bcc0fe08a
Nomethe <60054576+Nomethe@users.noreply.github.com>
2020-05-05 23:23:37 +0000

    Dice game
    new bug fix

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+var player1name = prompt("Player 1 name");
+
+var player2name = prompt("Player 2 name");
+
 var randomnumberofplayer1 = Math.floor(Math.random() * 6) + 1;
 
 var imges = "images/dice" + randomnumberofplayer1 + ".png";
@@ -13,14 +17,19 @@ var imges = "images/dice" + randomnumberofplayer2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", imges);
 
 
+document.querySelectorAll("p")[0].innerHTML = player1name;
+
+document.querySelectorAll("p")[1].innerHTML = player2name;
+
+
 
 if (randomnumberofplayer1 > randomnumberofplayer2) {
 
-    document.querySelector("h1").innerHTML = "Player 1 win !";
+    document.querySelector("h1").innerHTML = player1name + " win ! " + randomnumberofplayer1;
 
 } else if (randomnumberofplayer1 < randomnumberofplayer2) {
 
-    document.querySelector("h1").innerHTML = "Player 2 win !";
+    document.querySelector("h1").innerHTML = player2name + " win ! " + randomnumberofplayer2;
 
 } else {
 
