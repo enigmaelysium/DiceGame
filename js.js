28fc4b445d1ca13b7a1038030139861cd4619625
Nomethe <60054576+Nomethe@users.noreply.github.com>
2020-05-06 00:40:03 +0000

    fixing bugs

--- js.js
+++ js.js
@@ -2,20 +2,6 @@ var player1name = prompt("Player 1 name");
 
 var player2name = prompt("Player 2 name");
 
-var randomnumberofplayer1 = Math.floor(Math.random() * 6) + 1;
-
-var imges = "images/dice" + randomnumberofplayer1 + ".png";
-
-document.querySelectorAll("img")[0].setAttribute("src", imges);
-
-
-
-var randomnumberofplayer2 = Math.floor(Math.random() * 6) + 1;
-
-var imges = "images/dice" + randomnumberofplayer2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", imges);
-
 
 document.querySelectorAll("p")[0].innerHTML = player1name;
 
@@ -23,16 +9,37 @@ document.querySelectorAll("p")[1].innerHTML = player2name;
 
 
 
-if (randomnumberofplayer1 > randomnumberofplayer2) {
+function theGame() {
+    var randomnumberofplayer1 = Math.floor(Math.random() * 6) + 1;
 
-    document.querySelector("h1").innerHTML = player1name + " win ! " + randomnumberofplayer1;
+    var imges = "images/dice" + randomnumberofplayer1 + ".png";
 
-} else if (randomnumberofplayer1 < randomnumberofplayer2) {
+    document.querySelectorAll("img")[0].setAttribute("src", imges);
 
-    document.querySelector("h1").innerHTML = player2name + " win ! " + randomnumberofplayer2;
 
-} else {
 
-    document.querySelector("h1").innerHTML = "Drow!";
+    var randomnumberofplayer2 = Math.floor(Math.random() * 6) + 1;
+
+    var imges = "images/dice" + randomnumberofplayer2 + ".png";
+
+    document.querySelectorAll("img")[1].setAttribute("src", imges);
+
+
+
+
+    if (randomnumberofplayer1 > randomnumberofplayer2) {
+
+        document.querySelector("h1").innerHTML = player1name + " win ! " + randomnumberofplayer1;
+
+    } else if (randomnumberofplayer1 < randomnumberofplayer2) {
+
+        document.querySelector("h1").innerHTML = player2name + " win ! " + randomnumberofplayer2;
+
+    } else {
+
+        document.querySelector("h1").innerHTML = "Drow!";
+
+    }
+
 
 }
